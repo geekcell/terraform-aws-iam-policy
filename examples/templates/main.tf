d82202901575188a9d340ce7f8355d31e5bb49d8
Jerome Wolff <Ic3w0lf@users.noreply.github.com>
2023-03-23 13:40:08 +0100

    Add template for normal ECS deployment

--- examples/templates/main.tf
+++ examples/templates/main.tf
@@ -1,25 +1,25 @@
 module "codedeploy_policy" {
   source = "../../"
 
-  name = "web-app"
+  name      = "web-app"
   templates = [
     {
       name = "codedeploy/ecs-blue-green-deployment"
       vars = {
-        codedeploy_application_name      = "my-project"
+        codedeploy_app_name              = "my-project"
         codedeploy_deployment_group_name = "web-app"
 
         ecs_cluster_name = "my-project"
         ecs_service_name = "web-app"
 
-        task_definition_task_role_name           = "web-app"
-        task_definition_task_execution_role_name = "web-app-exec"
+        task_definition_task_role_name      = "web-app"
+        task_definition_execution_role_name = "web-app-exec"
       }
     },
     {
       name = "ecr/push-and-pull"
       vars = {
-        ecr_repository = "web-app"
+        ecr_repository_name = "web-app"
       }
     }
   ]
